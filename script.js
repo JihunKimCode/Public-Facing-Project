ab8087ae3841eed74e89ccde2784ceacdade9d9b
JihunKimCode <jiunkim8@gmail.com>
2023-12-05 04:15:16 -0500

    google form results

--- script.js
+++ script.js
@@ -248,3 +248,21 @@ function darkmode() {
     // Call header color change function after toggling dark mode
     handleHeaderColorChange();
 } 
+
+/*****************
+ *  Collapsible  *
+ *****************/
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
